56593e3be969573a1cde852ff97d89055887c041
Tudor Tocan <tudor.tocan@gmail.com>
2022-02-23 11:47:54 +0000

    initialise terraform and update gitignore

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -1,1 +1,12 @@
 # Terraform init - will download any required packages
+
+# provider aws
+provider "aws" {
+
+  # let terraform know which region
+  region = "eu-west-1"
+}
+
+# initialize with terraform
+
+# what do we want to launch (type of service)
